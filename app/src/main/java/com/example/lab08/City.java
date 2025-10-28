418e71b08d1b18d0799088ca9acc9db3f8a809a6
Moffat Muriithi <mmuriith@ualberta.ca>
2025-10-28 17:20:32 -0600

    City class

--- app/src/main/java/com/example/lab08/City.java
+++ app/src/main/java/com/example/lab08/City.java
@@ -4,6 +4,7 @@ public class City {
     private String city;
     private String province;
 
+    //City java class
     public City(String city, String province){
         this.city = city;
         this.province = province;
